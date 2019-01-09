a4f9e75828404f9bcfe0ec5bb2338cc0f91fa742
Jared Clark <helloredfoxes@gmail.com>
2019-01-09 13:04:33 -0700

    first commit

---
+++ Pixel.java
@@ -0,0 +1,5 @@
+public class Pixel {
+	int redValue;
+	int greenValue;
+	int blueValue;
+}
---
+++ ppmImage.java
@@ -0,0 +1,11 @@
+public class ppmImage {
+
+
+	String fileID;
+	int width;
+	int height;
+	int maxColorValue;
+
+	//structure to hold a list of pixels
+
+}
